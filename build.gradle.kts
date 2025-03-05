9b6195fae74e417c3d973a0650b7cbd5311f23a3
avichai321 <avichai321@gmail.com>
2025-03-05 09:59:33 +0200

    Erase Version.txt and clean bulid.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,40 +21,6 @@ repositories {
     mavenCentral()
 }
 
-//val projectVersionFile = file("version.txt")
-
-// Function to read and increment version
-//fun getNextVersion(): String {
-//    if (!projectVersionFile.exists()) {
-//        projectVersionFile.writeText("1.0.0")
-//    }
-
-//    val currentVersion = projectVersionFile.readText().trim()
-//    val versionParts = currentVersion.split(".").map { it.toInt() }.toMutableList()
-
-    // Increment the patch version
-//    versionParts[2] = versionParts[2] + 1
-
-//    val newVersion = versionParts.joinToString(".")
-//    projectVersionFile.writeText(newVersion)
-//    return newVersion
-//}
-
-// Set the project version dynamically
-//version = getNextVersion()
-
-// Ensure JAR file name matches repo/directory name
-//val jarName = rootProject.name
-
-//tasks.withType<Jar> {
-//    archiveBaseName.set(jarName)
-//    archiveVersion.set(version)
-//}
-
-//tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-//    archiveBaseName.set(jarName)
-//    archiveVersion.set(version)
-//}
 
 graalvmNative {
     binaries {
