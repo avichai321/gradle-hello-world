4ae32d68a2647a7e4ad5eee0d1f8f5bc45713f08
avichai <avichai321@github.com>
2025-03-06 19:32:50 +0000

    Bump version to 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 group = "com.ido"
 description = "HelloWorld"
 application.mainClass.set("com.ido.HelloWorld")
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
